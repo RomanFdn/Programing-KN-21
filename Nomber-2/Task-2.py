4391d879145ef667646f69720b7ec52e3fbbd63a
RomanFdn <fedunroman43@gmail.com>
2024-11-05 14:12:51 +0200

    first commit

---
+++ Nomber-2/Task-2.py
@@ -0,0 +1,9 @@
+while True:
+    msg = input("please input a msg: ")
+    if msg == "Добрий день":
+        print("Добрий день")
+    elif msg == "Слава україна":
+        print("Героям слава")
+    elif msg == "Па Па":
+        print("the end")
+        break
